be37bb8340827ee6653306c5c349699e8de0cc1e
zahra <Zahra@Zahras-MacBook-Air.local>
2022-05-04 11:46:07 -0400

    spread operator

---
+++ Spread/index.js
@@ -0,0 +1,49 @@
+const users = [{
+        name: 'Brook',
+        scores: 75,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 16
+    },
+    {
+        name: 'Alex',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'David',
+        scores: 75,
+        skills: ['HTM', 'CSS'],
+        age: 22
+    },
+    {
+        name: 'John',
+        scores: 85,
+        skills: ['HTML'],
+        age: 25
+    },
+    {
+        name: 'Sara',
+        scores: 95,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 26
+    },
+    {
+        name: 'Martha',
+        scores: 80,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 18
+    },
+    {
+        name: 'Thomas',
+        scores: 90,
+        skills: ['HTM', 'CSS', 'JS'],
+        age: 20
+    }
+]
+for ({ name, scores, skills, age }
+    of users) {
+    console.log(name, scores, skills, age);
+}
+const newUsers = users.filter(n => n.skills.length < 3);
+console.log(newUsers);
